72ddfb7ee85c8ff58211e9ee7624b7b6e642b019
Emel-H <emel-emel99@hotmail.com>
2023-10-07 22:58:12 +0200

    added create new post functionality

--- Post/post.js
+++ Post/post.js
@@ -1,28 +1,33 @@
 const noroffPostsUrl = "https://api.noroff.dev/api/v1/social/posts/";
 
-
 async function getPost(id){
     try {
         const token = localStorage.getItem('accessToken');
         if(token==null){
             document.location.href = '/index.html';
         }else{
-            const response = await fetch(noroffPostsUrl+id+"?_author=true&_comments=true", {
-                method: 'GET',
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                    Authorization: `Bearer ${token}`,
-                  },
-            });
-            const jsonReturn = await response.json();
-            
-            if(response.ok){
-                if(edit==="true"){
-                    setPostEdit(jsonReturn);
-                } else {
-                    setPostView(jsonReturn);
-                }
+            if(newEntry==="true"){
+                setPostNew();
+            }
+            else{
+                const response = await fetch(noroffPostsUrl+id+"?_author=true&_comments=true", {
+                    method: 'GET',
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const jsonReturn = await response.json();
                 
+                if(response.ok){
+                    
+                    if(edit==="true"){
+                        setPostEdit(jsonReturn);
+                    } else {
+                        setPostView(jsonReturn);
+                    }
+                    
+                }
             }
         }
     }
@@ -59,6 +64,77 @@ async function editPost(id){
         // catches errors both in fetch and response.json
         console.log(error);
     }
+}
+
+async function newPost(){
+    try {
+        const token = localStorage.getItem('accessToken');
+        if(token==null){
+            document.location.href = '/index.html';
+        }else{
+            const response = await fetch(noroffPostsUrl, {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: document.getElementById("postTitle").value,
+                    body: document.getElementById("postBody").value,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    Authorization: `Bearer ${token}`,
+                  },
+            });
+            const jsonReturn = await response.json();
+            if(response.ok){
+                document.location.href = '/profile/index.html';
+            }
+        }
+    }
+    catch (error) {
+        // catches errors both in fetch and response.json
+        console.log(error);
+    }
+}
+
+function setPostNew(jsonReturn){
+    const post = document.getElementById("singlePost");
+    const form = document.createElement("form");
+    form.onkeydown = "return event.key != 'Enter';";
+
+    const title = document.createElement("div");
+    title.className = "form-group my-2";
+    const titleLabel = document.createElement("label");
+    titleLabel.innerHTML = "Post Title:";
+    const postTitle = document.createElement("input");
+    postTitle.type = "text";
+    postTitle.className = "form-control";
+    postTitle.id= "postTitle";
+    title.append(titleLabel);
+    title.append(postTitle);
+
+    const body = document.createElement("div");
+    body.className = "form-group my-2";
+    const bodyLabel = document.createElement("label");
+    bodyLabel.innerHTML = "Post Text:";
+    const postBody = document.createElement("input");
+    postBody.type = "textarea";
+    postBody.className = "form-control";
+    postBody.id = "postBody";
+    body.append(bodyLabel);
+    body.append(postBody);
+
+    const submitNew =  document.createElement("button");
+    submitNew.type = "button";
+    submitNew.id = "submitNew";
+    submitNew.className = "btn btn-light w-100 btn-lg my-2 mb-5";
+    submitNew.innerHTML = "Submit New";
+    submitNew.addEventListener("click", (e) => {newPost();});
+
+    form.append(title);
+    form.append(body);
+    form.append(submitNew);
+
+    post.append(form);
+
 }
 
 function setPostEdit(jsonReturn){
@@ -132,7 +208,7 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 const edit = params.get("edit");
-const newPost = params.get("new");
+const newEntry = params.get("new");
 
 getPost(id);
 
